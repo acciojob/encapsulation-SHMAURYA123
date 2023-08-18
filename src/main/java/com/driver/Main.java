c3ceeb080a2c84e710b07547b8e482958bbbd3fa
SHMAURYA123 <142140724+SHMAURYA123@users.noreply.github.com>
2023-08-18 16:04:36 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
      RWOnly obj= new RWOnly();
-//      System.out.println(obj.name);   //name has private access in com.driver.RWOnly
-      obj.setname("IFFCO");
-      System.out.println(obj.getname());
+//      obj.name="kunal";  //name has private access in com.driver.RWOnly
+      obj.setName("kunal");
+      obj.getName();
 }
