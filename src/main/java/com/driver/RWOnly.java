35d8ecfd791ea4039420be86a2b71545ea1877ef
SHMAURYA123 <142140724+SHMAURYA123@users.noreply.github.com>
2023-08-18 16:20:03 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,11 +2,11 @@ package com.driver;
 
 public class RWOnly {
     private String name;
- public void setname(String name) {
+ public void setName(String name) {
         this.name=name; //setters
     }
     // getters
-    public String getname(){
-        return this.name;
+    public String getName(){
+        return name;
     }
 }
